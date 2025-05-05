d16053822361e5dd2c50c4b2ea2a4a95479a8067
shatkovskii_a_v <shatkovskii_a_v@goznak.ru>
2025-05-05 18:57:40 +0300

    Additional edit

--- src/main/java/ru/comfortsoft/TestForConvSoftApplication.java
+++ src/main/java/ru/comfortsoft/TestForConvSoftApplication.java
@@ -2,7 +2,13 @@ package ru.comfortsoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Класс TestForConvSoftApplication - точка запуска сервиса.
+ *
+ * @author shatkovskii_a_v
+ * @created 2025-05-05 project TestForConvSoft for
+ * @company Удобный Софт
+ */
 @SpringBootApplication
 public class TestForConvSoftApplication {
 
